006d99c8a95a68cefb1f8de7170c6a4c3a052f27
Adams Wever <adamswever122132@gmail.com>
2023-06-27 17:40:42 -0400

    Update navegacion.jsx

--- cliente/src/componentes/navegacion.jsx
+++ cliente/src/componentes/navegacion.jsx
@@ -4,7 +4,7 @@ export function Navegacion() {
   return (
     <div className='flex justify-between py-3'>
         <Link to='/tareas'>
-        <h1 className='font-bold text-3x1 mb-5' style={{fontSize:'30px'}}>App de Tareas</h1>
+        <h1 className='font-bold text-3x1 mb-5' style={{fontSize:'30px'}}>TaskNavigator</h1>
         </Link>
         <button className='bg-indigo-500 px-5 py-2 rounded-lg'>
         <Link to='/tareas-crear'>Crear Tarea</Link>
